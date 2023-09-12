09417317210ed2eb8c392af428ed5d1e92188254
Hs chaudhary <hschaudhary06@gmail.com>
2023-09-12 15:02:31 +0530

    Add workspace on gitHub

---
+++ src/main/java/JenkinsCalculator.java
@@ -0,0 +1,13 @@
+
+public class JenkinsCalculator {
+
+	//Method to add 2 numbers
+	public int addNumbers(int a,int b) {
+		return (a+b);
+	}
+	//Method to subtract 2 numbers
+	public int subtractNumbers(int a,int b) {
+		return (a-b);
+	}
+	
+}
